0ea748db7e3c348f27686877367c0f0ed5b47c51
Alex Feinberg <alexandernfeinberg@gmail.com>
2016-10-23 14:16:23 +0300

    shows round page

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -2,4 +2,22 @@ class RoundsController < ApplicationController
   def new
     @round = Round.new
   end
+
+  def create
+    @round = Round.new(round_params)
+
+    if @round.save
+      redirect_to @round
+    end
+  end
+
+  def show
+    @round = Round.find(params[:id])
+  end
+
+  private
+
+  def round_params
+    params.require(:round).permit(:bet)
+  end
 end
--- test/integration/black_jack_test.rb
+++ test/integration/black_jack_test.rb
@@ -7,9 +7,13 @@ class BlackJackTest < ActionDispatch::IntegrationTest
     assert page.has_content? "Your balance: 1000 roubles"
   end
 
-  test "can bet money" do
+  test "can play a round" do
     visit "/"
 
     fill_in 'round_bet', with: 50
+    click_on 'Create Round'
+
+    # assert page.has_content? "Your balance: 950 roubles"
+    assert page.has_content? "Your bet: 50 roubles"
   end
 end

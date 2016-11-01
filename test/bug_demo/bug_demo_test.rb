f1dddbf25134f899ae6a364a03e1d309444dedd9
Alex Feinberg <alexandernfeinberg@gmail.com>
2016-11-01 23:47:25 +0300

    adds another test

--- test/bug_demo/bug_demo_test.rb
+++ test/bug_demo/bug_demo_test.rb
@@ -1,12 +1,13 @@
 require 'test_helper'
 
 class BugDemoTest < ActiveSupport::TestCase
-  describe "the bug" do
+  describe "polymorphic associations" do
     before do
+      # make sure that the ID's start off at 1.
       DatabaseCleaner.clean
     end
 
-    it "happens when the id is 1" do
+    it "fails when the id is 1" do
       game = GameBuilderService.build_game
       game.save!
 
@@ -24,7 +25,31 @@ class BugDemoTest < ActiveSupport::TestCase
 
       # ID is 2. It works.
       assert_equal 1, round.hands.last.reload.cards.size
-      # ID is 1. Not working.
+      # ID is 1. Not working. Actual value here is 0.
+      assert_equal 1, round.hands.first.reload.cards.size
+    end
+
+    it "succeeds otherwise" do
+      ActiveRecord::Base.connection.execute("SELECT setval('hands_id_seq', 1000);")
+
+      game = GameBuilderService.build_game
+      game.save!
+
+      round = game.rounds.build
+      round.hands = [
+        build(:hand, cards: [
+          game.deck.pop
+        ]),
+        build(:hand, cards: [
+          game.deck.pop
+        ]),
+      ]
+
+      round.save!
+
+      # ID is 1002. It works.
+      assert_equal 1, round.hands.last.reload.cards.size
+      # ID is 10001. It works.
       assert_equal 1, round.hands.first.reload.cards.size
     end
   end
